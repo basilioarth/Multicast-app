2f50188e6629a41aa5416cd38699c4f87cc216cc
basilioarth <basilio.arth@gmail.com>
2021-05-03 21:39:05 -0300

    Realizando uma refatoração geral no código

--- src/client.py
+++ src/client.py
@@ -1,44 +1,34 @@
 import datetime
+import config
 import socket
 import struct
 import sys
 import os
 
-MULTICAST_GROUP = "224.14.0.244"
-MULTICAST_PORT_EXPRESSION = 1901
-MULTICAST_PORT_PING = 1902
-MULTICAST_BUFFER_SIZE_BYTES = 64
-
-multicast_group = (MULTICAST_GROUP, MULTICAST_PORT_EXPRESSION)
+multicast_group = (config.MULTICAST_GROUP, config.MULTICAST_PORT_EXPRESSION)
 
 def log(text):
     now = datetime.datetime.now().ctime()
     print("[{}] {}".format(now, text))
 
-# Create the UDP datagram socket and configure a few options
 log("Criando o socket de comunicação com o servidor")
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.settimeout(1)                                                  # Timeout in seconds
-ttl = struct.pack('b', 1)                                           # Set TTL to 1 hop (limits the network reach to local-only)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)    # Define socket options
+sock.settimeout(1)                                                  
+ttl = struct.pack('b', 1)                                           
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)    
 
-expression_string = bytes(input("Digite a expressão a ser calculada:"), "utf-8")
-
-log("Enviando a expressão...")
-try:
+while True:
+    expression_string = bytes(input("Digite a expressão a ser calculada:"), "utf-8")
+    log("Enviando a expressão...")
     sent = sock.sendto(expression_string, multicast_group)
     msg_count = 0
-    while True:
-        try:
-            recv_message, server_address = sock.recvfrom(MULTICAST_BUFFER_SIZE_BYTES)
-            if recv_message:
-                msg_count += 1
-        except socket.timeout:
-            if msg_count <= 0:
-                log("Nenhuma resposta foi recebida!")
-            break
-        else:
-            log('O resultado {} foi recebido do servidor {}'.format(recv_message.decode("utf-8"), server_address))
-finally:
-    log("Encerrando a aplicação cliente")
-    sock.close()
+    try:
+        recv_message, server_address = sock.recvfrom(config.MULTICAST_BUFFER_SIZE_BYTES)
+        if recv_message:
+            msg_count += 1
+    except socket.timeout:
+        if msg_count <= 0:
+            log("Nenhuma resposta foi recebida!")
+        break
+    else:
+        log('O resultado {} foi recebido do servidor {}'.format(recv_message.decode("utf-8"), server_address))
--- src/server.py
+++ src/server.py
@@ -1,15 +1,12 @@
 import threading
 import datetime
+import config
 import socket
 import struct
 import time
 import sys
 import os
 
-MULTICAST_GROUP = "224.14.0.244"
-MULTICAST_PORT_EXPRESSION = 1901
-MULTICAST_PORT_PING = 1902
-MULTICAST_BUFFER_SIZE_BYTES = 64
 responded = []
 
 def resolve_expression(expression_string):
@@ -33,15 +30,15 @@ def create_socket(multicast_group, server_address):
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
     return sock
 
-def multicast_initialize_server(pingsock, showAnotherServersResponse):
-    multicast_pingaddress = (MULTICAST_GROUP, MULTICAST_PORT_PING)
+def multicast_align_servers(pingsock, showAnotherServersResponse):
+    multicast_pingaddress = (config.MULTICAST_GROUP, config.MULTICAST_PORT_PING)
     server_id = 1
     msg_count = 0
     global responded 
     pingsock.sendto(b'PING', multicast_pingaddress)
     while True:
         try:
-            recv_message, sender_address = pingsock.recvfrom(MULTICAST_BUFFER_SIZE_BYTES)
+            recv_message, sender_address = pingsock.recvfrom(config.MULTICAST_BUFFER_SIZE_BYTES)
             if recv_message:
                 msg_count += 1
                 received_id = int(recv_message.decode("utf-8"))
@@ -56,7 +53,6 @@ def multicast_initialize_server(pingsock, showAnotherServersResponse):
             if msg_count <= 0:
                 log("Nenhum outro servidor respondeu", False)
             break
-    #pingsock.close()
     return server_id, responded
 
 def multicast_ping_send(pingsock):
@@ -64,38 +60,15 @@ def multicast_ping_send(pingsock):
     while True:
         time.sleep(1)
         responded.clear()
-        multicast_initialize_server(pingsock, False)
-
-'''
-def multicast_ping_send(pingsock):
-    multicast_pingaddress = (MULTICAST_GROUP, MULTICAST_PORT_PING)
-    #pingsock.sendto(b'PING', multicast_pingaddress)
-    msg_count = 0
-    while True:
-        time.sleep(1)
-        responded.clear()
-        pingsock.sendto(b'PING', multicast_pingaddress)
-        try:
-            recv_message, sender_address = pingsock.recvfrom(MULTICAST_BUFFER_SIZE_BYTES)
-            if recv_message:
-                msg_count += 1
-                received_id = int(recv_message.decode("utf-8"))
-                log('Servidor {}{} ativo'.format(recv_message.decode("utf-8"), sender_address), False)
-                responded.append(received_id)
-        except socket.timeout:
-            if msg_count <= 0:
-                log("Nenhum outro servidor respondeu", False)
-    pingsock.close()
-''' 
+        multicast_align_servers(pingsock, False)
 
 def multicast_ping_respond(server_id):
-    local_address = ("", MULTICAST_PORT_PING)
-    sock = create_socket(MULTICAST_GROUP, local_address)
+    local_address = ("", config.MULTICAST_PORT_PING)
+    sock = create_socket(config.MULTICAST_GROUP, local_address)
     while True:
-        recv_message, client_address = sock.recvfrom(MULTICAST_BUFFER_SIZE_BYTES)
+        recv_message, client_address = sock.recvfrom(config.MULTICAST_BUFFER_SIZE_BYTES)
         msg = recv_message.decode("utf-8")
         if msg == "PING":
-            #log("Respondendo a mensagem de verificação como servidor {}".format(server_id), True)
             id_bytestr = str(server_id).encode("utf-8")
             sock.sendto(id_bytestr, client_address)
 
@@ -103,7 +76,6 @@ def set_response_server(server_id, pingsock):
     global responded
     responded_now = responded
     responded_now.sort()
-    #log("Os seguintes servidores responderam: {}".format(responded), False)
     if server_id in responded_now:
         if responded.index(server_id) == 0:
             log("O servidor detentor do id {} responderá".format(server_id), False)
@@ -118,7 +90,7 @@ pingsock.settimeout(0.5)
 ttl = struct.pack('b', 1)                                               
 pingsock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
-server_id, responded = multicast_initialize_server(pingsock, True)
+server_id, responded = multicast_align_servers(pingsock, True)
 if server_id == None:
     log("Falha ao definir o id do servidor", False)
     exit(1)
@@ -127,32 +99,28 @@ time.sleep(1)
 log("Definindo o id do servidor para: {}".format(server_id), False)
 time.sleep(1)
 
-# Create the UDP socket and bind it to network interface
 time.sleep(1)
 log("Criando um socket para a resolução da expressão", False)
 time.sleep(1)
 
-server_address = ('', MULTICAST_PORT_EXPRESSION)
-sock = create_socket(MULTICAST_GROUP, server_address)
+server_address = ('', config.MULTICAST_PORT_EXPRESSION)
+sock = create_socket(config.MULTICAST_GROUP, server_address)
 time.sleep(1)
 log("O servidor agora está aguardando pelo envio da expressão", False)
 time.sleep(1)
 
-# Create the respond socket
 time.sleep(1)
-log("Criando um socket de comunicação entre servidores para o servidor {}".format(server_id), False)
+log("Criando um socket de comunicação entre servidores para o servidor {}\n".format(server_id), False)
 time.sleep(1)
+
 ping_responding_thread_send = threading.Thread(target=multicast_ping_send, args=[pingsock])
 ping_responding_thread_send.start()
 ping_responding_thread_response = threading.Thread(target=multicast_ping_respond, args=[server_id])
 ping_responding_thread_response.start()
 
-#log("O socket de comunicação entre servidores do servidor {} está ouvindo...".format(server_id))
-
 while True:
-    recv_message, client_address = sock.recvfrom(MULTICAST_BUFFER_SIZE_BYTES)
-    log("Requisição recebida de [{}]: {}".format(client_address, recv_message), True)
-    # Check with other servers who will respond the requisition
+    recv_message, client_address = sock.recvfrom(config.MULTICAST_BUFFER_SIZE_BYTES)
+    log("Requisição recebida de [{}]: {}".format(client_address, recv_message.decode("utf-8")), True)
     should_respond = set_response_server(server_id, pingsock)
     if should_respond:
         log("Calculando o resultado da expressão...", False)
